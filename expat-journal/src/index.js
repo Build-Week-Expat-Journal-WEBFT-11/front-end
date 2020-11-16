bd6676ef6109f9333914b191d0eda607a309d622
Cameron Garner <camerongarner9@gmail.com>
2020-11-16 18:43:18 -0500

    basic posts rendering complete,started on the creat post form

--- expat-journal/src/index.js
+++ expat-journal/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+import reducer from "./Unit3Components/reducers"
+
+let store = createStore(reducer,applyMiddleware(thunk))
+console.log(store.getState())
 
 ReactDOM.render(
-  <App />,
- document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+    document.getElementById('root')
+  );
+  
 
 
